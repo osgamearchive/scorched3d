9dca047544fa874b28fd27f2e87b144b5ac94f12
b0bir0v <>
2006-03-04 06:25:22 +0000

    Added avatars to recent events page.

--- scorchedstats/recentevents.php
+++ scorchedstats/recentevents.php
@@ -10,7 +10,7 @@ $seriesid = getIntParameter('Series');
 
 <?
 // Query player events and player/weapon names
-$query ="SELECT scorched3d_events.playerid, scorched3d_events.weaponid, scorched3d_events.eventtime, scorched3d_events.eventtype, scorched3d_events.otherplayerid, (playernames.name) as playername, (otherplayernames.name) as otherplayername, (scorched3d_weapons.name) as weaponname, (scorched3d_weapons.armslevel) as armslevel FROM scorched3d_events LEFT JOIN scorched3d_players playernames ON scorched3d_events.playerid=playernames.playerid LEFT JOIN scorched3d_players otherplayernames ON scorched3d_events.otherplayerid=otherplayernames.playerid LEFT JOIN scorched3d_weapons ON scorched3d_events.weaponid=scorched3d_weapons.weaponid where scorched3d_events.prefixid=$prefixid and scorched3d_events.seriesid=$seriesid ORDER BY eventtime desc limit 100";
+$query ="SELECT scorched3d_events.playerid, scorched3d_events.weaponid, scorched3d_events.eventtime, scorched3d_events.eventtype, scorched3d_events.otherplayerid, (playernames.name) as playername, (otherplayernames.name) as otherplayername, (playernames.avatarid) as avatarid, (otherplayernames.avatarid) as otheravatarid, (scorched3d_weapons.name) as weaponname, (scorched3d_weapons.armslevel) as armslevel FROM scorched3d_events LEFT JOIN scorched3d_players playernames ON scorched3d_events.playerid=playernames.playerid LEFT JOIN scorched3d_players otherplayernames ON scorched3d_events.otherplayerid=otherplayernames.playerid LEFT JOIN scorched3d_weapons ON scorched3d_events.weaponid=scorched3d_weapons.weaponid where scorched3d_events.prefixid=$prefixid and scorched3d_events.seriesid=$seriesid ORDER BY eventtime desc limit 100";
 $result = mysqlQuery($query) or die("Query failed : " . mysql_error());
 $row = mysql_fetch_object($result);
 ?>
@@ -40,14 +40,26 @@ while ($row = mysql_fetch_object($result))
 	case 7: $event = 'yellow>connected'; break;
 	case 8: $event = 'yellow>disconnected'; break;
 	}
-	$otherplayer = "<a href=playerstats.php?Prefix=".$prefixid."&Series=".$seriesid."&PlayerID=".$row->otherplayerid.">".$row->otherplayername."</a>";
+	
 	switch ($row->eventtype)
 	{
+	case 1:
+	case 2: $otherplayer = "<table><tr><td align=center><img border=0 src='getbinary.php?id=".$row->otheravatarid."'></td><td><a href=playerstats.php?Prefix=".$prefixid."&Series=".$seriesid."&PlayerID=".$row->otherplayerid.">".$row->otherplayername."</a></td></tr></table>"; break;
 	case 3: $otherplayer = "<font color=violet><center>with</center></font>"; break;
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+	case 8: $otherplayer = ''; break;
 	}
         echo "<tr>";
         echo "<td>$row->eventtime</td>";
-        echo "<td align=right><a href=playerstats.php?Prefix=".$prefixid."&Series=".$seriesid."&PlayerID=".$row->playerid.">".$row->playername."</a></td>";
+        echo "<td>";
+		echo "<table><tr>";
+		echo "<td align=center><img border=0 src='getbinary.php?id=".$row->avatarid."'></td>";
+		echo "<td><a href=playerstats.php?Prefix=".$prefixid."&Series=".$seriesid."&PlayerID=$row->playerid>$row->playername</a></td>";
+		echo "</td>";
+		echo "</tr></table>";
         echo "<td align=center><font color=".$event."</font></td>";
         echo "<td>$otherplayer</td>";
         echo "<td><a href=weaponstats.php?Prefix=".$prefixid."&Series=".$seriesid."&WeaponID=".$row->weaponid.">".$row->weaponname."</a></td>";
